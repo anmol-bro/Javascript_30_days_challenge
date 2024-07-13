edce8853a33855aec6f894f95a8bf524505de2c2
Anmol Kumar Pandey <76219206+anmol-bro@users.noreply.github.com>
2024-07-13 19:26:00 +0000

    day1 task completed

---
+++ 01_variables_and_datatypes.js
@@ -0,0 +1,28 @@
+var userName = 'Anmol'
+let age = 10
+const userLogin = true
+
+const myObj = {
+    username :"Anmolkumar",
+    subject :"js"
+}
+
+const myArr = [1,2,4,5]
+
+// console.log(userName)
+console.log(age)
+// console.log(userLogin)
+
+
+console.log(typeof(userName))
+console.log(typeof(age))
+console.log(typeof(userLogin))
+console.log(typeof(myObj))
+console.log(typeof(myArr))
+
+age = 15
+console.log(age)
+
+
+//  myArr = [4,5,6] //error observation
+//  console.log(myArr)
