84cc3cb51c58adcf55e607d706a310179b9d7ad0
Anmol Kumar Pandey <76219206+anmol-bro@users.noreply.github.com>
2024-07-22 21:06:22 +0000

    array_continue

---
+++ 06_arrays.js
@@ -0,0 +1,10 @@
+const arr1 = [1,2,3,4,5]
+console.log(arr1);
+
+console.log(arr1[0]);
+console.log(arr1[arr1.length - 1]); 
+//arr1.push(8); //push operation to append the number in array
+//arr1.pop() ; //pop operation to delete the last element in the array
+//arr1.unshift(8);
+//arr1.shift();
+console.log(arr1)
